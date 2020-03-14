18bdbd475e88a40a24c23437e11d799f4f3d3524
z451343010 <1178214954@qq.com>
2020-03-14 21:44:20 +0800

    test commit

---
+++ src/Test2.java
@@ -0,0 +1,10 @@
+public class Test2 {
+    public static void main(String args[]){
+        int a = 0;
+        if (a == 2) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
