3abbb534d7c21bfbb94f2e9f7177ce6f7a02aa56
z451343010 <1178214954@qq.com>
2020-03-18 22:46:55 +0800

    bitcoin

---
+++ src/Bitcoin.java
@@ -0,0 +1,34 @@
+public class Bitcoin {
+
+    // 计算攻击者成功概率的方法
+    public static double attackerSuccessProbability(double q, int z){
+
+        double p = 1.0 - q;  //定义一个
+        double lambda = z * (q/p);
+        double sum = 1.0;
+        int i, k;
+        for (k = 0; k <= z; k++) {
+            double poisson = Math.exp(-lambda);
+            for (i = 1; i<=k; i++){
+                poisson *= lambda / i;
+            }
+            sum -= poisson * (1 - Math.pow(q/p,z-k));
+        }
+        return p;
+    }
+
+    public static void main(String args[]){
+        double z = attackerSuccessProbability(0.1,9);
+        System.out.println(z);
+    }
+
+//    public void loop(){
+//        for(double loopNum = 1.0 ; loopNum <= 10.0; loopNum++){
+//            double z = 0.0;
+//            z = attackerSuccessProbability()
+//            System.out.println();
+//        }
+//    }
+
+
+}
